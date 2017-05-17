a65ed55cc470a6b6668c76f2c7a199e447995f01
John Doe <john@sample.com>
2017-05-17 16:22:03 +0530

    Added scr files

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='restutils',
+    version='0.1dev',
+    packages=['restutils'],
+    url='https://github.com/pupattan/restutils',
+    license='',
+    author='pulakpattanayak',
+    author_email='pulak.pattanayak@gamil.com',
+    description='Provides libraries for REST API applications'
+)
